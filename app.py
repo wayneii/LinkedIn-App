c559b7aeb19df062570635c15af49c5b327d8a14
wayneii <95460389+wayneii@users.noreply.github.com>
2022-12-11 23:06:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,6 @@ observation = [income, education, parent, married, gender, age]
 
 with st.form("key1"):
            submit = st.form_submit_button("Load Prediction")
-           pred_outcome = lr.predict([obversvation])
+           pred_outcome = lr.predict([observation])
            st.write(pred_outcome)
            
