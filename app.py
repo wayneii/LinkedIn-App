10653321f2eebffb72eb464504ac1740b9697366
wayneii <95460389+wayneii@users.noreply.github.com>
2022-12-11 21:48:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ income = st.selectbox("Income Level",
                         "50 to under $75,000",
                         "75 to under $100,000",
                         "100 to under $150,000",
-                        "$150,000 or more"])
+                        "$150,000 or more"], format_func=lambda x: income[x])
 
-income.index(income)
+print(income)
 
 #If income != "" & education != "" & parent != "" married != "" 
   #s = pd.read_csv("social_media_usage.csv")
