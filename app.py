b296aa005f3520bfd16471df7a24abb30d7e3165
wayneii <95460389+wayneii@users.noreply.github.com>
2022-12-11 20:36:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title("Let's see if I can predict if you used LinkedIn")
 
 #Age
-#age = st.slider(label="Enter you age", min_value=1, max_value=98,value=7)
-#st.write("Your age is: ", age)
+age = st.slider(label="Enter you age", min_value=1, max_value=98,value=7)
+st.write("Your age is: ", age)
 
 
 #Gender
@@ -23,9 +23,9 @@ st.title("Let's see if I can predict if you used LinkedIn")
 #                         "High school graduate (Grade 12 with diploma or GED certificate)",
 #                         "Some college, no degree (includes some community college)",
 #                         "Two-year associate degree from a college or university",
-#                         "Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)",
+#                         "Four-year college or university degree/Bachelorâ€™s degree (e.g., BS, BA, AB)",
 #                         "Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)",
-#                         "Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)"])
+#                         "Postgraduate or professional degree, including masterâ€™s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)"])
 ##Income
 #income = st.selectbox("Income Level", 
 #              options = ["Less than $10,000",
