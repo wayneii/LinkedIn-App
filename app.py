418935ede164204453ce5b0cfd3ab9056685679c
wayneii <95460389+wayneii@users.noreply.github.com>
2022-12-12 23:20:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -106,10 +106,10 @@ with st.form("key1"):
                       pred_outcome_probability = lr.predict_proba([observation])
                       if pred_outcome == [1]:
                                  st.write("You are a LinkedIn User")
-                                 st.text(pred_outcome_probability)
+                                 st.text(pred_outcome_probability[0][1])
                       else:
                                  st.write("You are not a LinkedIn User")
-                                 st.text(pred_outcome_probability)
+                                 st.text(pred_outcome_probability[0][1])
                       #st.text("The probability you're a LinkedIn user is:", pred_outcome_probability)
                       st.text(pred_outcome)
 
